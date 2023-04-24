463286d288d897260dfa07f4750176fd8ddd534e
Brandon T <98200133+btomecsko@users.noreply.github.com>
2023-04-24 16:31:26 -0500

    made tweaks to appointment validation

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,21 +2,11 @@ class Appointment < ApplicationRecord
   belongs_to :pet
   belongs_to :doctor
 
-  #validate :valid_date?
-
-  # validates :appt_time, inclusion: { in: (Time.current.beginning_of_day + 7.hours)..(Time.current.beginning_of_day + 19.hours), message: "Appointments can only be scheduled between 7am and 7pm." }
   validates :appt_time, uniqueness: { scope: :doctor_id, message: "This time slot is already booked." }
 
   def date_format
     self.appt_time.strftime("%B %d, %Y at %l:%M%p")
   end
 
-  private
-
-  # def valid_date?
-  #   if Appointment.where(appt_time: self.appt_time)
-  #     errors.add(:appt_time, "Slot no longer available")
-  #   end
-  # end
 
 end
